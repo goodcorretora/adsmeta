3649451f5f9df04a99eb3df6e68b3a83d86af0d6
Antonio Carlos <goodcorretora@gmail.com>
2022-09-05 22:00:58 -0300

    First component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,16 @@
+import icon from '../../assets/img/notification-icon.svg';
+import './styles.css';
+
+type Props = {
+    saleId: number;
+}
+
+
+function NotificationButton( {saleId} : Props ) {
+    return(
+        <div className="dsmeta-red-btn" >
+            <img src={icon} alt="Notificar" />
+        </div>
+    )
+}
+  export default NotificationButton;
